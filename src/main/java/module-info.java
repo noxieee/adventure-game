e0c34c11268243f6addd47200fc4ba49be4de610
Vojtěch Keder <kedv00@vse.cz>
2023-11-06 18:13:42 +0100

    Opraveny chyby při spouštění aplikace, bylo potřeba upravit soubor module-info.java.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,5 +2,6 @@ module cz.vse.java.kedv00.adventura {
     requires javafx.controls;
     requires javafx.fxml;
 
-
+    opens cz.vse.java.kedv00.adventura to javafx.fxml;
+    exports cz.vse.java.kedv00.adventura;
 }
